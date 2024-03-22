9986bb2bb6fc7fdec633f2b5602febf3f7329980
Qian Liping <carrotphoto@qq.com>
2024-03-22 22:30:31 +0800

    s2617, unable to solve this problem

---
+++ src/main/java/com/qianliping/leetcode/s2617/Solution.java
@@ -0,0 +1,16 @@
+package com.qianliping.leetcode.s2617;
+
+/**
+ * no idea to solve this problem
+ *
+ * @author Qian Liping
+ * @see <a href="https://leetcode.cn/problems/minimum-number-of-visited-cells-in-a-grid/">2617.网格图中最少访问的格子数</a>
+ * @since 2024/3/22
+ */
+public class Solution {
+
+    public int minimumVisitedCells(int[][] grid) {
+
+        return -1;
+    }
+}
